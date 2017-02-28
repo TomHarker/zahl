4f1c5e32229a09c63f7872e4094d9b17aee1939a
zahllang <zahl.language@gmail.com>
2017-02-28 18:08:10 +0000

    Update preprocessor.py

--- interpreter/preprocessor.py
+++ interpreter/preprocessor.py
@@ -1,1 +1,9 @@
-
+# Write function to open file and preprocess.
+def preprocess(filename):
+  source = open(filename, "r")
+  
+  # Find any import statements and replace with relevant files.
+  
+  # Remove comments.
+  
+  # Remove unnecessary whitespace.
