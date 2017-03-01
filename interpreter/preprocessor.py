4f7106df890a7f3db380403b23cbafd11d17ee33
zahllang <zahl.language@gmail.com>
2017-03-01 08:28:27 +0000

    Update preprocessor.py

--- interpreter/preprocessor.py
+++ interpreter/preprocessor.py
@@ -2,7 +2,12 @@
 def preprocess(filename):
   source = open(filename, "r")
   
+  # Remove comments.
+  
   # Find any import statements and replace with relevant files.
+  for line in source :
+     if "import" in line:
+        line = line.replace('.' , '')
   
   # Remove comments.
   
