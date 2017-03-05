f102beea9dee0250ce77db22c01f0ff643de96b5
TomHarker <damasa@hotmail.co.uk>
2017-03-05 17:29:26 +0000

    Add generic file reader function

--- interpreter/preprocessor.py
+++ interpreter/preprocessor.py
@@ -1,14 +1,30 @@
+# opens file and returns list of lines
+def readFile(filename):
+
+    source = []
+
+    try:
+        source = open(filename).read().splitlines()
+    except:
+        print 'ERROR: {} does not exist'.format(filename)
+
+    # return non-empty lines
+    return filter(None, source)
+
+
+
 # Write function to open file and preprocess.
 def preprocess(filename):
-  source = open(filename, "r")
-  
-  # Remove comments.
-  
-  # Find any import statements and replace with relevant files.
-  for line in source :
-     if "import" in line:
-        line = line.replace('.' , '')
-  
-  # Remove comments.
-  
-  # Remove unnecessary whitespace.
+    source = readFile(filename)
+
+    # Remove comments.
+
+    # Find any import statements and replace with relevant files.
+    for line in source:
+        if "import" in line:
+            line = line.replace('.' , '')
+
+
+    # Remove unnecessary whitespace.
+
+
